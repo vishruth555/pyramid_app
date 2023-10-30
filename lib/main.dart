6508848900a78d3943d97c2e4d727777e5cca30d
vishruth <vishruth555@gmail.com>
2023-10-30 16:44:54 +0530

    added top and bottom navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,8 +11,56 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.tealAccent,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'pyramid demo',
+      theme:
+          new ThemeData(primarySwatch: Colors.green, canvasColor: Colors.green),
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade100,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.menu_rounded),
+          actions: [
+            Padding(
+                padding: EdgeInsets.only(right: 20),
+                child: Icon(Icons.notifications_none_rounded))
+          ],
+        ),
+        bottomNavigationBar: ClipRRect(
+          child: BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+            child: BottomNavigationBar(
+              backgroundColor: Colors.transparent,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_filled), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search_rounded), label: 'Product'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.compare_arrows_rounded),
+                    label: 'Transaction'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.perm_identity_rounded), label: 'Account'),
+              ],
+            ),
+          ),
+        ),
+        body: const Column(
+          children: [
+            // header text
+
+            //big card
+
+            //best plans which is side scrollable
+
+            //investment guide which is vertical scrollable
+
+            //bottom navigation
+          ],
+        ),
+      ),
     );
   }
 }
